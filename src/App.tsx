34c3c7f5c9fcf8d934c7e7f83e7036d755f3a2e7
ouifi <ouifidev@gmail.com>
2021-04-20 11:29:13 -0400

    Improved api failure message on front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { Container, Button, Row, Col } from 'react-bootstrap';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -61,13 +62,21 @@ function App() {
                         </>
                         : (
                             hasError
-                                ? <>
-                                    <p>
-                                        Something has gone wrong.
-                                        The site might be experiencing high traffic, or perhaps there is a bug preventing the page from loading.
-                                        You may attempt to try again by pressing the button below, but please do not spam it.
-                                    </p>
-                                </>
+                                ? <Container fluid style={{ padding: "50px" }}>
+                                    <Row>
+                                        <Col md={3} />
+                                        <Col md={6}>
+                                            <p>
+                                                Something has gone wrong.
+                                                The site might be experiencing high traffic, or perhaps there is a bug preventing the page from loading.
+                                                You may attempt to try again by pressing the button below, but please do not spam it.
+                                            </p>
+                                        </Col>
+                                    </Row>
+                                    <Button onClick={newGame} size="lg">
+                                        Reload
+                                    </Button>
+                                </Container>
                                 : <div style={{ minHeight: "400px" }}>
                                     <Spinner size="xl" className="ml-auto mr-auto spinner-spaced" />
                                 </div>
