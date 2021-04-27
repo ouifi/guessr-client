3feae94c071a90c8e542569a2b045d4cd404a730
ouifi <ouifidev@gmail.com>
2021-04-26 22:01:28 -0400

    Added simple Icon framework

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -5,16 +5,16 @@ import './Icon.css';
 const sizes = {
     sm: {
         width: 10,
-        height: 10
+        // height: 10
     },
     md: {
-        width: 30,
-        height: 30,
+        // width: 30,
+        // height: 30,
     }
 };
 
-const Icon = ({ children, size }: { children: React.ReactNode, size?: "sm" | "md" }) => {
-    return <div className="guessr-icon" style={{...sizes[size || "md"]}}>
+const Icon = ({ children, size, onClick }: { children: React.ReactNode, size?: "sm" | "md", onClick?: () => void }) => {
+    return <div className="guessr-icon" style={{...sizes[size || "md"]}} onClick={onClick}>
         {children}
     </div>;
 };
--- src/components/Icon/src/Hamburger.tsx
+++ src/components/Icon/src/Hamburger.tsx
@@ -5,8 +5,8 @@
 // Minor changes are made removing unneeded props
 import React from 'react';
 
-const Hamburger = () => {
-    return <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+const Hamburger = ({ wide, width, height }: { wide?: boolean, width?: number, height?: number }) => {
+    return <svg width={wide ? "90%" : (width ? width : "")} height={height} preserveAspectRatio="none" viewBox="0 0 448 512">
         <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
         </path>
     </svg>;
